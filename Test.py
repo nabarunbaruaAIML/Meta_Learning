b823512fd5742a6eee5d454e0fe0acb2d3afbb7a
Nabarun Barua <64695833+nabarunbaruaAIML@users.noreply.github.com>
2022-04-23 22:05:19 +0530

    Minnor Change

--- Test.py
+++ Test.py
@@ -1,9 +1,12 @@
 from transformers import   AutoModelForSequenceClassification
-
+from transformers import pipeline
 
 def main():
     model = AutoModelForSequenceClassification.from_pretrained('Artifact/Model')
     print(model)
+    model_checkpoint = "Artifact\Model"
+    token_classifier = pipeline( "sentiment-analysis", model=model_checkpoint )
+    print(token_classifier("My name is Sylvain and I work at Hugging Face in Brooklyn."))
     
 if __name__=="__main__":
     main()
